ad28488de20a50d7ada88518db11f584c1963e8f
mark <mark@dbswebsite.com>
2016-08-14 13:59:10 -0400

    Testing weather module in console

--- Modules/weather.js
+++ Modules/weather.js
@@ -2,6 +2,7 @@ var weather = require('weather-js');
 
 var exports = {};
 
+
 function getWeather(){
 	return new Promise( function( resolve, reject ){
 		var args = {
@@ -10,13 +11,21 @@ function getWeather(){
 		};
 
 		weather.find(args, function(error, result) {
+ 
 			if( error ){
+				console.log(err);
 				reject(error);
 			} else {
+				// Returns: Location, Current, Forecast
+				console.log( result[0]['current']['temperature'] );
+				console.log( result[0]['current']['skytext'] );
+  				//console.log(JSON.stringify(result, null, 2));
 				resolve(result);
 			}
 		});
 	});
 }
+
+getWeather();
 
 module.exports = exports;
